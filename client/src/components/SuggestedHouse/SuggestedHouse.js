05f8b829f5ff689c22b224d8056d2e10793ea334
unknown <chotwilaiwan@gmail.com>
2022-06-15 17:18:09 +0700

    refactor: refac SuggestedHouse component

--- client/src/components/SuggestedHouse/SuggestedHouse.js
+++ client/src/components/SuggestedHouse/SuggestedHouse.js
@@ -1,13 +1,35 @@
 import classes from "./SuggestedHouse.module.scss";
+import { FcHome } from "react-icons/fc";
 import ListedHouse from "../../UI/Section/ListedHouse/ListedHouse";
 const Houses = ["h1", "h2", "h3"];
 const SuggestedHouse = () => {
   return (
     <div className={classes.SuggestedHouse}>
+      <div className={classes.text}>
+        <div className={classes.topic}>
+          <div className={classes.bigCircle}>
+            <div className={classes.smallCircle}>
+              <FcHome className={classes.icon} />
+            </div>
+          </div>
+          <span>โครงการแนะนำ</span>
+        </div>
+        <div className={classes.detail}>
+          <div className={classes.line} />
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam
+            excepturi consectetur mollitia quisquam sit aut veritatis
+            voluptatibus numquam delectus.
+          </p>
+        </div>
+      </div>
       <div className={classes.box}>
         {Houses.map((e, i) => {
           return <ListedHouse data={e} />;
         })}
+      </div>
+      <div className={classes.buttonBox}>
+        <div className={classes.button}>ดูโครงการทั้งหมด</div>
       </div>
     </div>
   );
