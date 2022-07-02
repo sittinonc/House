180ed89aeb3e56cbc21293bd70c76401dfd61603
sittinon chotwilaiwan <chotwilaiwan@gmail.com>
2022-07-02 06:46:17 -0700

    feat: init allProject

---
+++ client/src/UI/SideWidget/SideWidget.js
@@ -0,0 +1,11 @@
+import classes from "./SideWidget.module.scss";
+
+const SideWidget = () => {
+  return (
+    <div className={classes.container}>
+      <div className={classes.sth}></div>
+    </div>
+  );
+};
+
+export default SideWidget;
--- client/src/pages/AllProjects/AllProjects.js
+++ client/src/pages/AllProjects/AllProjects.js
@@ -1,9 +1,43 @@
 import classes from "./AllProjects.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
+import ShowHouse from "../../UI/ShowHouse/ShowHouse";
+
+const Houses = ["h1", "h2", "h3"];
 const AllProjects = () => {
   return (
-    <div className={classes.container}>
-      <div className={classes.sth}></div>
+    <div className={classes.page}>
+      <div className={classes.container}>
+        <div className={classes.wrapper}>
+          <h1 className={classes.head}>โครงการทั้งหมด</h1>
+        </div>
+        <div className={classes.wrapper}>
+          <div className={classes.attributesSet}>
+            <div style={{ marginLeft: "5%" }} className={classes.eachAttribute}>
+              <span>โครงการ</span>
+              <FontAwesomeIcon className={classes.icon} icon={faChevronDown} />
+            </div>
+            <div className={classes.eachAttribute}>
+              <span>พื้นที่</span>
+              <FontAwesomeIcon className={classes.icon} icon={faChevronDown} />
+            </div>
+            <div className={classes.eachAttribute}>
+              <span>ราคาสูงไปต่ำ</span>
+              <FontAwesomeIcon className={classes.icon} icon={faChevronDown} />
+            </div>
+          </div>
+        </div>
+        <div className={classes.house}>
+          <div className={classes.box}>
+            {Houses.map((e, i) => {
+              return <ShowHouse data={e} />;
+            })}
+          </div>
+        </div>
+      </div>
+
+      <div className={classes.sideWidgetBox}></div>
     </div>
   );
 };
