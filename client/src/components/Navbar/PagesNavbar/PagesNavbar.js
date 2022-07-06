ca0d9c76fd53d5ade23f18f62a8221026e368f61
sittinon chotwilaiwan <chotwilaiwan@gmail.com>
2022-07-06 23:17:52 +0700

    feat add peagesNav desktop design

---
+++ client/src/components/Navbar/PagesNavbar/PagesNavbar.js
@@ -0,0 +1,59 @@
+import { Link } from "react-router-dom";
+import classes from "./PagesNavbar.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FaUserCircle } from "react-icons/fa";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { faHouseChimney } from "@fortawesome/free-solid-svg-icons";
+
+const PagesNavbar = (props) => {
+  return (
+    <div className={classes.container}>
+      <div className={classes.navbar}>
+        <div className={classes.flexNav}>
+          <div className={classes.left}>
+            <div className={classes.brand}>
+              <FontAwesomeIcon id={classes.logo} icon={faHouseChimney} />
+              <h2>QHouse</h2>
+            </div>
+          </div>
+          <div className={classes.mid}>
+            <div className={classes.menu}>
+              <Link className={classes.navLink} to="/home">
+                หน้าแรก
+              </Link>
+              <Link className={classes.navLink} to="/allprojects">
+                โครงการ
+              </Link>
+              <Link className={classes.navLink} to="/allprojects">
+                ซื้อ
+              </Link>
+              <Link className={classes.navLink} to="/allprojects">
+                ติดต่อเรา
+              </Link>
+            </div>
+          </div>
+          <div className={classes.right}>
+            <div className={classes.content}>
+              <BsFillTelephoneFill
+                className={classes.icon}
+                style={{ fontSize: "17px" }}
+              />{" "}
+              <span>0961728911</span>
+              <Link
+                style={{ textDecoration: "none", color: "white" }}
+                to="/admin"
+              >
+                <FaUserCircle
+                  className={classes.icon}
+                  style={{ marginLeft: "18px" }}
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PagesNavbar;
--- client/src/pages/AllProjects/AllProjects.js
+++ client/src/pages/AllProjects/AllProjects.js
@@ -2,7 +2,10 @@ import classes from "./AllProjects.module.scss";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { BsFillGrid3X3GapFill } from "react-icons/bs";
+import { FaBars } from "react-icons/fa";
 
+import PagesNavbar from "../../components/Navbar/PagesNavbar/PagesNavbar";
 import ShowHouse from "../../UI/ShowHouse/ShowHouse";
 
 //widgets
@@ -10,9 +13,6 @@ import SideWidget from "../../UI/SideWidget/SideWidget";
 import Measurement from "../../components/Widgets/Measurement";
 import Utility from "../../components/Widgets/Utility";
 import Reccommend from "../../components/Widgets/Reccommend/Reccommend";
-
-import { BsFillGrid3X3GapFill } from "react-icons/bs";
-import { FaBars } from "react-icons/fa";
 
 const AllProjects = () => {
   let count = { gridCount: 0, landscapeCount: 0 };
@@ -41,158 +41,175 @@ const AllProjects = () => {
   const [dropdownCommand, setDropdownCommand] = useState(null);
 
   return (
-    <div className={classes.page}>
-      <div className={classes.container}>
-        <div className={classes.wrapper}>
-          <h1 className={classes.head}>โครงการทั้งหมด</h1>
-          <div className={classes.attributesSet}>
-            <div style={{ marginLeft: "5%" }} className={classes.eachAttribute}>
-              <div
-                className={classes.label}
-                onClick={() => {
-                  if (dropdownCommand === "location") {
-                    setDropdownCommand(null);
-                  } else {
-                    setDropdownCommand("location");
-                  }
-                }}
-              >
-                <span>{location}</span>
-                <FontAwesomeIcon className={classes.icon} icon={faAngleDown} />
-              </div>
-              <div
-                className={
-                  dropdownCommand === "location"
-                    ? classes.dropdownActive
-                    : classes.dropdownNone
-                }
-              >
-                {locationArray.map((e, i) => {
-                  return (
-                    <div
-                      key={i}
-                      onClick={() => {
-                        setLocation(e);
+    <div className={classes.x}>
+      <div className={classes.page}>
+        <PagesNavbar />
+        <div className={classes.inPage}>
+          <div className={classes.container}>
+            <div className={classes.wrapper}>
+              <h1 className={classes.head}>โครงการทั้งหมด</h1>
+              <div className={classes.attributesSet}>
+                <div
+                  style={{ marginLeft: "5%" }}
+                  className={classes.eachAttribute}
+                >
+                  <div
+                    className={classes.label}
+                    onClick={() => {
+                      if (dropdownCommand === "location") {
                         setDropdownCommand(null);
-                      }}
-                      className={classes.dropdownItem}
-                    >
-                      <span>{e}</span>
-                    </div>
-                  );
-                })}
+                      } else {
+                        setDropdownCommand("location");
+                      }
+                    }}
+                  >
+                    <span>{location}</span>
+                    <FontAwesomeIcon
+                      className={classes.icon}
+                      icon={faAngleDown}
+                    />
+                  </div>
+                  <div
+                    className={
+                      dropdownCommand === "location"
+                        ? classes.dropdownActive
+                        : classes.dropdownNone
+                    }
+                  >
+                    {locationArray.map((e, i) => {
+                      return (
+                        <div
+                          key={i}
+                          onClick={() => {
+                            setLocation(e);
+                            setDropdownCommand(null);
+                          }}
+                          className={classes.dropdownItem}
+                        >
+                          <span>{e}</span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+                <div className={classes.eachAttribute}>
+                  <div
+                    className={classes.label}
+                    onClick={() => {
+                      if (dropdownCommand === "project") {
+                        setDropdownCommand(null);
+                      } else {
+                        setDropdownCommand("project");
+                      }
+                    }}
+                  >
+                    <span>{project}</span>
+                    <FontAwesomeIcon
+                      className={classes.icon}
+                      icon={faAngleDown}
+                    />
+                  </div>
+                  <div
+                    className={
+                      dropdownCommand === "project"
+                        ? classes.dropdownActive
+                        : classes.dropdownNone
+                    }
+                  >
+                    {projectArray.map((e, i) => {
+                      return (
+                        <div
+                          key={i}
+                          onClick={() => {
+                            setProject(e);
+                            setDropdownCommand(null);
+                          }}
+                          className={classes.dropdownItem}
+                        >
+                          <span>{e}</span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+                <div className={classes.eachAttribute}>
+                  <div
+                    className={classes.label}
+                    onClick={() => {
+                      if (dropdownCommand === "sort") {
+                        setDropdownCommand(null);
+                      } else {
+                        setDropdownCommand("sort");
+                      }
+                    }}
+                  >
+                    <span>{sort}</span>
+                    <FontAwesomeIcon
+                      className={classes.icon}
+                      icon={faAngleDown}
+                    />
+                  </div>
+                  <div
+                    className={
+                      dropdownCommand === "sort"
+                        ? classes.dropdownActive
+                        : classes.dropdownNone
+                    }
+                  >
+                    {sortArray.map((e, i) => {
+                      return (
+                        <div
+                          key={i}
+                          onClick={() => {
+                            setSort(e);
+                            setDropdownCommand(null);
+                          }}
+                          className={classes.dropdownItem}
+                        >
+                          <span>{e}</span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+                <div className={classes.display}>
+                  <div
+                    className={classes.each}
+                    onClick={() => {
+                      count.landscapeCount = count.landscapeCount + 1;
+                    }}
+                  >
+                    <FaBars />
+                  </div>
+                  <div
+                    className={classes.each}
+                    onClick={() => {
+                      count.gridCount = count.gridCount + 1;
+                    }}
+                  >
+                    <BsFillGrid3X3GapFill />
+                  </div>
+                </div>
               </div>
             </div>
-            <div className={classes.eachAttribute}>
-              <div
-                className={classes.label}
-                onClick={() => {
-                  if (dropdownCommand === "project") {
-                    setDropdownCommand(null);
-                  } else {
-                    setDropdownCommand("project");
-                  }
-                }}
-              >
-                <span>{project}</span>
-                <FontAwesomeIcon className={classes.icon} icon={faAngleDown} />
-              </div>
-              <div
-                className={
-                  dropdownCommand === "project"
-                    ? classes.dropdownActive
-                    : classes.dropdownNone
-                }
-              >
-                {projectArray.map((e, i) => {
-                  return (
-                    <div
-                      key={i}
-                      onClick={() => {
-                        setProject(e);
-                        setDropdownCommand(null);
-                      }}
-                      className={classes.dropdownItem}
-                    >
-                      <span>{e}</span>
-                    </div>
-                  );
+            <div className={classes.house}>
+              <div className={classes.box}>
+                {Houses.map((e, i) => {
+                  return <ShowHouse data={e} />;
                 })}
-              </div>
-            </div>
-            <div className={classes.eachAttribute}>
-              <div
-                className={classes.label}
-                onClick={() => {
-                  if (dropdownCommand === "sort") {
-                    setDropdownCommand(null);
-                  } else {
-                    setDropdownCommand("sort");
-                  }
-                }}
-              >
-                <span>{sort}</span>
-                <FontAwesomeIcon className={classes.icon} icon={faAngleDown} />
-              </div>
-              <div
-                className={
-                  dropdownCommand === "sort"
-                    ? classes.dropdownActive
-                    : classes.dropdownNone
-                }
-              >
-                {sortArray.map((e, i) => {
-                  return (
-                    <div
-                      key={i}
-                      onClick={() => {
-                        setSort(e);
-                        setDropdownCommand(null);
-                      }}
-                      className={classes.dropdownItem}
-                    >
-                      <span>{e}</span>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-            <div className={classes.display}>
-              <div
-                className={classes.each}
-                onClick={() => {
-                  count.landscapeCount = count.landscapeCount + 1;
-                }}
-              >
-                <FaBars />
-              </div>
-              <div
-                className={classes.each}
-                onClick={() => {
-                  count.gridCount = count.gridCount + 1;
-                }}
-              >
-                <BsFillGrid3X3GapFill />
               </div>
             </div>
           </div>
-        </div>
-        <div className={classes.house}>
-          <div className={classes.box}>
-            {Houses.map((e, i) => {
-              return <ShowHouse data={e} />;
-            })}
-          </div>
-        </div>
-      </div>
 
-      <div className={classes.sideWidgetBox}>
-        {/*Utility*/}
-        <Utility />
-        {/*หน่วยวัดพื้นที่*/}
-        <Measurement />
-        {/*Reccommend*/}
-        <Reccommend />
+          <div className={classes.sideWidgetBox}>
+            {/*Utility*/}
+            <Utility />
+            {/*หน่วยวัดพื้นที่*/}
+            <Measurement />
+            {/*Reccommend*/}
+            <Reccommend />
+          </div>
+        </div>
       </div>
     </div>
   );
