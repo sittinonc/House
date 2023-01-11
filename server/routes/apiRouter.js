f1972001ed0d005eac0790b5f480ec01f8ca5ee7
Grapeka <chotwilaiwan@gmail.com>
2023-01-11 18:21:48 +0700

    `refactor` : create controllers folder

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const router = express.Router()
-const apiController = require('./apiController')
-const checkAuth = require("./checkAuth");
+const express = require('express');
+const router = express.Router();
+const apiController = require('../controllers/apiController');
+const checkAuth = require('./checkAuth');
 
-router.post('/post', checkAuth, apiController.PostHouse)
-router.post('/patch', checkAuth, apiController.PatchHouse)
-router.get('/list', apiController.ListHouse)
-router.get('/findbyname/:name', apiController.FindHouse)
-router.get('/search/', apiController.SearchHouse)
-router.get('/suggest', apiController.ListSuggest)
-router.post('/interest', apiController.InterestHouse)
-router.post('/subscribe', apiController.Subscribe)
-router.delete('/delete/:_id', checkAuth, apiController.DeleteHouse)
+router.post('/post', checkAuth, apiController.PostHouse);
+router.post('/patch', checkAuth, apiController.PatchHouse);
+router.get('/list', apiController.ListHouse);
+router.get('/findbyname/:name', apiController.FindHouse);
+router.get('/search/', apiController.SearchHouse);
+router.get('/suggest', apiController.ListSuggest);
+router.post('/interest', apiController.InterestHouse);
+router.post('/subscribe', apiController.Subscribe);
+router.delete('/delete/:_id', checkAuth, apiController.DeleteHouse);
 
-module.exports = router
+module.exports = router;
--- server/routes/imageRouter.js
+++ server/routes/imageRouter.js
@@ -1,13 +1,18 @@
-const express = require("express");
-const router = express.Router()
-const imageController = require('./imageController')
-const multer = require('multer')
-const checkAuth = require('./checkAuth')
+const express = require('express');
+const router = express.Router();
+const imageController = require('../controllers/imageController');
+const multer = require('multer');
+const checkAuth = require('./checkAuth');
 
 // router.post('/upload/multiple', multer().array('file', 5), imageController.UploadMultiple)
-router.get('/view/:filename', imageController.View)
-router.get('/list', imageController.List)
-router.post('/upload/single', checkAuth, multer().single('file'), imageController.UploadSingle)
-router.delete('/delete/:filename', checkAuth, imageController.Delete)
+router.get('/view/:filename', imageController.View);
+router.get('/list', imageController.List);
+router.post(
+  '/upload/single',
+  checkAuth,
+  multer().single('file'),
+  imageController.UploadSingle
+);
+router.delete('/delete/:filename', checkAuth, imageController.Delete);
 
-module.exports = router
+module.exports = router;
