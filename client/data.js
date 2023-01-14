8ae443b014f2ccbcb32f01084d03373edfee7b82
Grapeka <chotwilaiwan@gmail.com>
2023-01-14 15:29:09 +0700

    `feat` : json to obj data

---
+++ client/data.js
@@ -0,0 +1,51 @@
+const aHouse = {
+  _id: 'ab123',
+  price: '555',
+  name: 'บ้านเดี่ยว นครปฐม...',
+  status: 'กำลังก่อสร้าง',
+  houseDetails: {
+    price: '5000000',
+    utility: {
+      area: '80',
+      usableArea: '55',
+      bedroom: '3',
+      bathRoom: '3',
+      parkingLot: '2',
+      others: 'ติดแอร์ทุกห้องนอน มีเฟอร์นิเจอร์พร้อมอยู่',
+    },
+  },
+  location: {
+    province: {
+      id: 4,
+      name: 'นครปฐม',
+    },
+    district: {
+      id: 1,
+      name: 'เมืองนครปฐม',
+    },
+    subDistrict: {
+      id: 1,
+      name: 'บางกระสอ',
+    },
+    zipCode: '102991',
+    road: 'ถนน นคร.',
+    moreDetails: 'ห่างกับโลตัสนครปฐม 2km, ติดกับบ้านเดี่ยว...',
+    latlon: {
+      latitude: '123123123',
+      longitude: '88312300',
+    },
+  },
+  photos: {
+    filenames: ['', '', ''],
+    blueprints: ['', ''],
+  },
+  buildingInformation: {
+    start: '12/05/2021',
+    finish: '05/03/2022',
+  },
+  websiteInfo: {
+    isSuggest: false,
+    createAt: '10/03/2022',
+    lastedEdited: '21/06/2022',
+  },
+};
